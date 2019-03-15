ac320ddf9f6f88747d5ce67d9e90bcf346a97791
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-03-14 22:40:22 -0400

    Added Error Handler global

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 
 // GLOBAL CONST
 const INGREDIENT_PRICES = {
@@ -166,4 +168,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
